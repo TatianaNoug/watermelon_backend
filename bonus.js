c63f08f074a4113e977f96cb53cd9bf9edab15d2
TatianaNoug <tatianameister.tm@gmail.com>
2019-10-27 01:27:58 +0200

    bonus main ok

---
+++ bonus.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+var wallets = require('./bonusWallet');
+var payins = require('./bonusPayin');
+var payouts = require('./bonusPayout');
+
+let db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "watermelon",
+    port: "8889"
+});
+
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+})
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(function (req, res, next) {
+    if("x-auth-token" in req.headers){
+        let access_token = req.headers["x-auth-token"];
+        let query = `SELECT users.id, users.email, users.is_admin, wallets.id as wallet_id FROM users JOIN wallets on users.id = wallets.user_id WHERE api_key = '${access_token}'`;
+
+        req.db.query(query, function (err, result, fields) {
+            if(err) throw err;
+            if(result.length > 0){
+                req.user = {
+                    id: result[0].id,
+                    email: result[0].email,
+                    is_admin: false,
+                    wallet_id: result[0].wallet_id
+                };
+                next();
+            }else{
+                res.status(401).send();
+            }
+        });
+    }else{
+        res.status(401).send();
+    }
+});
+
+app.use('/v1/wallets', wallets);
+app.use('/v1/payins', payins);
+app.use('/v1/payouts', payouts);
+
+
+
+app.listen(8000, function () {
+    console.log("Watermelon app listening on port 8000");
+});
+
